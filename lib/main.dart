36a82f83ce5e634c397565a54614c771add4ae07
akshai <57423537+nyouibou@users.noreply.github.com>
2024-03-16 20:38:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
+}
+
+class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  const MyApp({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera Interface',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CameraScreen(camera: camera),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraScreen({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Camera Interface')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CameraPreview(_controller);
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[200],
+              child: Center(
+                  // child: Text(
+                  //   'Your UI components here',
+                  //   style: TextStyle(fontSize: 18.0),
+                  // ),
+                  ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed action here
+        },
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
